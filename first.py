172c915af8476b19687bbf4d709a2f7d50a5f32d
PassQuestion <99644257+PassQuestion@users.noreply.github.com>
2022-02-14 18:56:43 +0800

    Update first.py

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as t
-from numpy import *                                                                                                                                                    t.rcParams['font.family']=['FreeSans']
+from numpy import *
+
+'''start'''
+t.rcParams['font.family']=['FreeSans']
 a=loadtxt("./temp.txt")
-t.plot(a, marker = "H")                       
+t.plot(a, marker = "H")
+t.legend(labels=["2020max","2020min","2021max","2021min"])
 t.title("the temperature from 1.30-2.8")
 t.xlabel("days")
 t.ylabel("temperature/°C")
